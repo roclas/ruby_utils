bf289f362eb766a453bec856f4c6d096b646a84e
carlos <carlopolo@gmail.com>
2014-03-07 12:58:51 +0100

    Create convert_table_to_csv.py

---
+++ convert_table_to_csv.py
@@ -0,0 +1,15 @@
+require 'nokogiri'
+
+file_name, second, third = ARGV
+
+file = File.open(file_name, "r")
+table_string = file.read
+file.close
+doc = Nokogiri::HTML(table_string)
+
+doc.xpath('//table//tr').each do |row|
+  row.xpath('td').each do |cell|
+    print '"', cell.text.gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), "\", "
+  end
+  print "\n"
+end
