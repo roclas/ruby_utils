6eac260675690a8547a3dad9d6603e4c1b930c41
carlos <carlopolo@gmail.com>
2014-03-07 13:30:50 +0100

    Update convert_table_to_csv.py
    removing blanks

--- convert_table_to_csv.py
+++ convert_table_to_csv.py
@@ -9,7 +9,7 @@ doc = Nokogiri::HTML(table_string)
 
 doc.xpath('//table//tr').each do |row|
   row.xpath('td').each do |cell|
-    print '"', cell.text.gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), "\", "
+    print '"', cell.text.strip().gsub("\n", ' ').gsub('"', '\"').gsub(/(\s){2,}/m, '\1'), "\", "
   end
   print "\n"
 end
